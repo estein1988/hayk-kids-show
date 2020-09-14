df42537e2664210e6457a2dacc535170d1916bac
estein1988 <estein1988@gmail.com>
2020-09-14 14:45:02 -0600

    Complete assessment.

--- app/controllers/kids_controller.rb
+++ app/controllers/kids_controller.rb
@@ -1,18 +1,26 @@
 class KidsController < ApplicationController
+  
   def index
     @kids = Kid.all
-
     render json: { kids: @kids }
   end
 
   def create
-    @kid = Kid.create({
-      username: params[:kid][:username],
-      description: params[:kid][:description],
-      favorite_color: params[:kid][:favorite_color],
-      age: params[:kid][:age],
-    })
+    @kid = Kid.new(kid_params)
+    
+    if @kid.valid?
+      @kid.save
+      render json: { kid: @kid }
+    else 
+      render json: @kid.errors.full_messages 
+    end
 
-    render json: { kid: @kid }
   end
+
+  private
+
+  def kid_params
+    params.require(:kid).permit(:username, :age, :description, :favorite_color)
+  end
+
 end
--- app/models/kid.rb
+++ app/models/kid.rb
@@ -1,2 +1,4 @@
 class Kid < ApplicationRecord
+    validates :username, presence: true, uniqueness: true, length: {minimum: 7, maximum: 25}
+    validates :age, presence: true, numericality: {greater_than_or_equal_to: 6, less_than_or_equal_to: 15}
 end
